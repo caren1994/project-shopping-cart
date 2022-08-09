939edae4d1c632eb39a41456eca2294c9790eb6c
caren <carenoliveirasilvapontes@gmail.com>
2022-08-09 14:59:20 -0500

    start no projeto

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,8 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+ it('1.1 Verifique se fetchProducts é uma função',()=>{
+  expect(typeof fetchProducts).toBe('function');
+ });
+  
 });
